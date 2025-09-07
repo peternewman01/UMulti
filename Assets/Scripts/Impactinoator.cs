18a2af47fb9bb0e02e6b0876c01d0fcb9da15427
PeterNewman445 <40744513+PeterNewman445@users.noreply.github.com>
2025-09-07 18:55:11 -0400

    implemented impact vfx

---
+++ Assets/Scripts/Impactinoator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Impactinoator : MonoBehaviour
+{
+    [SerializeField] private GameObject vfxPrefab;
+    [SerializeField] private float vfxLifetime = 2f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+            return;
+
+        //approximate contact point and normal using closest point
+        Vector3 contactPoint = other.ClosestPoint(transform.position);
+        Vector3 hitDirection = (contactPoint - transform.position).normalized;
+
+        //face opposite the hit direction
+        Quaternion rotation = Quaternion.LookRotation(-hitDirection);
+
+        GameObject vfx = Instantiate(vfxPrefab, contactPoint, rotation);
+        Destroy(vfx, vfxLifetime);
+    }
+}
