43789d92c8553878b8dd06f9d8712210d45696a3
PeterNewman445 <40744513+PeterNewman445@users.noreply.github.com>
2025-09-09 23:13:22 -0400

    added footstep vfx implementation, new tree, and updated generator for tree2

---
+++ Assets/Scripts/PlayerAnimationEvents.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class PlayerAnimationEvents : MonoBehaviour
+{
+    [SerializeField] private GameObject stepVFX;
+    [SerializeField] private Vector3 vfxOffset = new Vector3(0,0,0);
+    public void Step()
+    {
+        Debug.Log("Step fired");
+        GameObject step = Instantiate(stepVFX, (transform.parent.position - vfxOffset), Quaternion.identity);
+        Destroy(step, .3f);
+    }
+}
