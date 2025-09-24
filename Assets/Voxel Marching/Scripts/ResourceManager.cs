e2706c4cabda29858f4444ca35cb4f51b78c0c3f
XavOlmst <xavierolmst@gmail.com>
2025-09-24 12:55:09 -0400

    resources have scriptable objects now, resources are still spawning

--- Assets/Voxel Marching/Scripts/ResourceManager.cs
+++ Assets/Voxel Marching/Scripts/ResourceManager.cs
@@ -1,3 +1,4 @@
+using Palmmedia.ReportGenerator.Core;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
@@ -8,15 +9,7 @@ using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class ResourceManager : MonoBehaviour
-{
-    [Serializable]
-    private struct Resource
-    {
-        public List<GameObject> resourcePrefab;
-        public bool isNetworkObject;
-        public FitnessData spawningData;
-    }
-
+{ 
     [SerializeField] private List<Resource> resourceGenerators = new();
 
     private void Start()
@@ -29,33 +22,6 @@ public class ResourceManager : MonoBehaviour
         ChunkMananger.Instance.ChunkFinishedGenerating -= OnChunkFinishUpdate;
     }
 
-    private void SpawnResource(Resource resource, Vector3 pos)
-    {
-        pos.y = ChunkMananger.Instance.GetChunkHeight() * ChunkMananger.Instance.GetSpacing();
-        pos.x += Random.Range(-resource.spawningData.posOffset, resource.spawningData.posOffset);
-        pos.z += Random.Range(-resource.spawningData.posOffset, resource.spawningData.posOffset);
-        if (Physics.Raycast(new Ray(pos, Vector3.down), out RaycastHit hitData, 10000, ~LayerMask.NameToLayer("Ground")))
-        {
-            if (Mathf.Clamp01(ResourceFitness(pos.x, pos.z, hitData, resource.spawningData)) < resource.spawningData.density)
-            {
-                pos.y = hitData.point.y;
-                GameObject spawned = null;
-                int prefabIndex = Random.Range(0, resource.resourcePrefab.Count);
-                if (resource.isNetworkObject)
-                {
-                    ServerFunctions.SpawnObjectServerRpc(resource.resourcePrefab[prefabIndex], out spawned);
-                }
-                else
-                {
-                    spawned = Instantiate(resource.resourcePrefab[prefabIndex], ChunkMananger.Instance.ResourceParent.transform);
-                }
-
-                spawned.transform.position = pos;
-                spawned.transform.parent = ChunkMananger.Instance.ResourceParent.transform;
-            }
-        }
-    }
-
     public void OnChunkFinishUpdate(Vector2Int chunk)
     {
         for (int x = 0; x < ChunkMananger.Instance.GetChunkSize(); x++)
@@ -64,24 +30,95 @@ public class ResourceManager : MonoBehaviour
             {
                 foreach (Resource resource in resourceGenerators)
                 {
-                    SpawnResource(resource, ChunkMananger.Instance.ChunkToWorld(chunk) + new Vector3(x, 0, z));
+                    resource.SpawnResource(ChunkMananger.Instance.ChunkToWorld(chunk) + new Vector3(x, 0, z));
                 }
             }
         }
     }
+}
 
-    private float ResourceFitness(float x, float z, RaycastHit hitData, FitnessData data)
+[CreateAssetMenu(fileName = "New Resource", menuName = "Resource-Generation/Resource")]
+public class Resource : ScriptableObject
+{
+    [SerializeField] private List<WeightedPrefab> weightedPrefabs;
+    [SerializeField] private bool isNetworkObject;
+    [SerializeField] private FitnessData spawningData;
+    private int totalWeight = 0;
+
+    private void OnValidate()
+    {
+        totalWeight = 0;
+        foreach(var weight in weightedPrefabs)
+        {
+            totalWeight += weight.weight;
+        }
+    }
+
+    public void SpawnResource(Vector3 pos)
+    {
+        pos.y = ChunkMananger.Instance.GetChunkHeight() * ChunkMananger.Instance.GetSpacing();
+        pos.x += Random.Range(-spawningData.posOffset, spawningData.posOffset);
+        pos.z += Random.Range(-spawningData.posOffset, spawningData.posOffset);
+        if (Physics.Raycast(new Ray(pos, Vector3.down), out RaycastHit hitData, 10000, ~LayerMask.NameToLayer("Ground")))
+        {
+            if (Mathf.Clamp01(ResourceFitness(pos.x, pos.z, hitData)) < spawningData.density)
+            {
+                pos.y = hitData.point.y;
+                GameObject spawned = null;
+                GameObject prefab = GetRandomPrefab();
+                if (isNetworkObject)
+                {
+                    ServerFunctions.SpawnObjectServerRpc(prefab, out spawned);
+                }
+                else
+                {
+                    spawned = Instantiate(prefab, ChunkMananger.Instance.ResourceParent.transform);
+                }
+
+                spawned.transform.position = pos;
+                spawned.transform.parent = ChunkMananger.Instance.ResourceParent.transform;
+            }
+        }
+    }
+
+    private float ResourceFitness(float x, float z, RaycastHit hitData)
     {
         float fitness = Mathf.PerlinNoise(x + ChunkMananger.Instance.Seed, z + ChunkMananger.Instance.Seed);
         //Debug.Log($"Perlin at ({x}, {z}) = {fitness}");
-        fitness += Random.Range(-data.randomness, data.randomness);
-        if (Mathf.Acos(Vector3.Dot(hitData.normal, Vector3.up)) > data.maxSteepness) fitness += 0.9f;
-        if (Mathf.Acos(Vector3.Dot(hitData.normal, Vector3.up)) < data.minSteepness) fitness += 0.9f;
-        if (hitData.point.y > data.maxHeight) fitness += 0.7f;
-        if (hitData.point.y < data.minHeight) fitness += 0.7f;
+        fitness += Random.Range(-spawningData.randomness, spawningData.randomness);
+        if (Mathf.Acos(Vector3.Dot(hitData.normal, Vector3.up)) > spawningData.maxSteepness) fitness += 0.9f;
+        if (Mathf.Acos(Vector3.Dot(hitData.normal, Vector3.up)) < spawningData.minSteepness) fitness += 0.9f;
+        if (hitData.point.y > spawningData.maxHeight) fitness += 0.7f;
+        if (hitData.point.y < spawningData.minHeight) fitness += 0.7f;
 
         //Debug.Log($"ResourceFitness at ({x}, {z}) = {fitness}");
         return fitness;
+    }
+
+    public GameObject GetResourcePrefabByWeight(int weight)
+    {
+        if (weight < 0) return null;
+        int curWeight = 0;
+
+        foreach(var weightedResource in weightedPrefabs)
+        {
+            curWeight += weightedResource.weight;
+            if (curWeight > weight)
+                return weightedResource.prefab;
+        }
+
+        return null;
+    }
+
+    public int GetWeightByPercent(float percent)
+    {
+        percent = Mathf.Clamp01(percent);
+        return (int)(totalWeight * percent);
+    }
+
+    public GameObject GetRandomPrefab()
+    {
+        return GetResourcePrefabByWeight(Random.Range(0, totalWeight));
     }
 
     [Serializable]
@@ -95,4 +132,11 @@ public class ResourceManager : MonoBehaviour
         public float maxHeight;
         public float minHeight;
     }
+}
+
+[Serializable]
+public struct WeightedPrefab
+{
+    public GameObject prefab;
+    public int weight;
 }
